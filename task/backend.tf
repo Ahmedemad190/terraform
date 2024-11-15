b1f8e6a7bafa1c3db6a8054ad0fb2b6690b3b099
Ahmedemad190 <130619786+Ahmedemad190@users.noreply.github.com>
2024-11-15 22:05:52 +0300

    Delete task/backend.tf

--- task/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-test123"        
-    key            = "gloable/project/my-terraform.tfstate"     
-    region         = "us-east-1"                       
-    dynamodb_table = "terraform-lock-table"             
-    
-  }
-}
